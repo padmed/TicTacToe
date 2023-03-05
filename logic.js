afa53387e403455f5ab5343092b1de060ee7cdf0
padmed <ddarchinian@gmail.com>
2023-03-05 17:01:58 +0400

    Add touchButton module

--- logic.js
+++ logic.js
@@ -3,4 +3,48 @@ const GameBoard = (function () {
   let gameBoard = [];
 })();
 
-const displayController = (function () {})();
+const touchButton = (function () {
+  const onTouchStart = (event) => {
+    event.target.classList.add("buttonActive");
+  };
+
+  const onTouchEnd = (event) => {
+    event.target.classList.remove("buttonActive");
+  };
+
+  const active = function (button) {
+    button.addEventListener("touchstart", onTouchStart);
+    button.addEventListener("touchend", onTouchEnd);
+  };
+
+  const inert = function (button) {
+    button.removeEventListener("touchstart", onTouchStart);
+    button.removeEventListener("touchend", onTouchEnd);
+  };
+
+  return {
+    active,
+    inert,
+  };
+})();
+
+const displayController = (function () {
+  const screen = document.body;
+  const startGameButton = document.querySelector("#start");
+
+  const startGame = function () {
+    touchButton.active(startGameButton);
+    // touchButton.inert(startGameButton);
+
+    startGameButton.onclick = function () {
+      screen.classList.remove("greetScreen");
+      touchButton.inert(startGameButton);
+    };
+  };
+
+  return {
+    startGame,
+  };
+})();
+
+displayController.startGame();
