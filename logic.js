e67c93e095bc6f9237551aa24db1929e1dcfdb48
padmed <ddarchinian@gmail.com>
2023-03-22 20:17:03 +0400

    Add checkWin method in GameBoard object

--- logic.js
+++ logic.js
@@ -136,6 +136,34 @@ const GameBoard = (function () {
     }
   };
 
+  const checkWin = function () {
+    const winningCombinations = [
+      // horizontal
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      // vertical
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      // diagonal
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+
+      if (
+        gameBoard[a] !== null &&
+        gameBoard[a] === gameBoard[b] &&
+        gameBoard[a] === gameBoard[c]
+      ) {
+        return [gameBoard[a]];
+      }
+    }
+  };
+
   const renderShape = function (squareIndex) {
     const square = document.getElementById(squareIndex);
     const crossIcon = document.createElement("img");
@@ -169,6 +197,7 @@ const GameBoard = (function () {
     setPlayerShapes,
     makeMove,
     getBoard,
+    checkWin,
   };
 })();
 
